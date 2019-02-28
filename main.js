f73e2df99101843055c1760f27e7200b5b3119d1
Edin Purivatra <edinpurivatra@gmail.com>
2019-02-28 13:26:35 +0100

    changed source

--- main.js
+++ main.js
@@ -1,52 +1,68 @@
-let groups = [{ name: 'sega',  price: 23,  halfYearDiscountPercentage: 10 },
- { name: 'mega',  price: 69.71,  halfYearDiscountPercentage: 20 },
- { name: 'giga', price: 105.44,  halfYearDiscountPercentage: 25} ];
+let groups = [{
+    name: 'sega',
+    price: 23,
+    halfYearDiscountPercentage: 10
+  },
+  {
+    name: 'mega',
+    price: 69.71,
+    halfYearDiscountPercentage: 20
+  },
+  {
+    name: 'giga',
+    price: 105.44,
+    halfYearDiscountPercentage: 25
+  }
+];
 
-let users = [
-  { name: 'salmin', subscription: 'giga' },
-  { name: 'elvir', subscription: 'sega' },
-  { name: 'edin', subscription: 'mega' },
-  { name: 'almir', subscription: 'sega' },
-  { name: 'mersad', subscription: 'giga' },
-  { name: 'aladin', subscription: null }
- ];
-// average monthly income
-let avgMonthReduce = (acc, val) => {
-  for(let i = 0; i < users.length; i++){
-    if(val.name == users[i].subscription){
-      acc += val.price;
-    }
+let users = [{
+    name: 'salmin',
+    subscription: 'giga'
+  },
+  {
+    name: 'elvir',
+    subscription: 'sega'
+  },
+  {
+    name: 'edin',
+    subscription: 'mega'
+  },
+  {
+    name: 'almir',
+    subscription: 'sega'
+  },
+  {
+    name: 'mersad',
+    subscription: 'giga'
+  },
+  {
+    name: 'aladin',
+    subscription: null
   }
-  return acc;
-} 
-// count only users that have subscription
-let countActiveUsers = (acc, val) => {
-  if(val.subscription != null){
-    acc++;
-  }
-  return acc;
-}
-// adds halfYearPrice to users
+];
+
+let transformed = groups.reduce((result, group) => {
+  result[group.name] = group.price;
+  return result;
+}, {});
+
+console.log(transformed);
+
+let avgMonth = users.reduce((total, subs) => total += subs.subscription != null ? transformed[subs.subscription] : 0, 0)
+
+console.log(avgMonth);
+
+let avgMonthPerUser = avgMonth / users.reduce((counter, user) => counter += user.subscription != null ? 1 : 0, 0);
+
+console.log(avgMonthPerUser);
+
 let halfYearPrice = (element) => {
-  let newObj = {
-    name: element.name,
-    subscription: element.subscription,
-    halfYearPrice: null,
-  };
-  for(let i = 0; i < groups.length; i++){
-    if(element.subscription == groups[i].name){
-      newObj.halfYearPrice=groups[i].price*6-(groups[i].price*6*groups[i].halfYearDiscountPercentage/100);
-    }
+  for (let i = 0; i < groups.length; i++) {
+    element.halfYearPrice = element.subscription == groups[i].name ? groups[i].price * 6 - (groups[i].price * 6 * groups[i].halfYearDiscountPercentage / 100) : null;
   }
-  return newObj;
+  return element;
 }
-
-let avgMonth = groups.reduce(avgMonthReduce,0);
-let avgMonthPerUser = avgMonth / users.reduce(countActiveUsers,0);
-
-
-console.log('Prosjecna mjesecna zarada iznosi ' + avgMonth);
-console.log('Prosjecna mjesecna zarada po korisniku iznosi ' + avgMonthPerUser);
 
 users = users.map(halfYearPrice);
+
 console.log(users);
